86610a5324e75871fd65fde1577d61b160bd6276
giKallergis <gkallergis7@gmail.com>
2024-10-21 20:47:45 +0200

    chemlm-molbert analysis

--- code/visualizations/molb_chemlm_analysis.py
+++ code/visualizations/molb_chemlm_analysis.py
@@ -45,16 +45,15 @@ class Chemlm_model(nn.Module):
         inter_med=384
         set_seed_all(seed_)
         config=RobertaConfig.from_pretrained('{}'.format(mo_path),output_hidden_states=True,output_attentions=True,num_hidden_layers = n_layers,num_attention_heads = att)
-        self.roberta = RobertaModel.from_pretrained('{}'.format(mo_path),config=config)  
-        for param in self.roberta.parameters():
+        self.bert = RobertaModel.from_pretrained('{}'.format(mo_path),config=config)  
+        for param in self.bert.parameters():
            param.requires_grad=False
         self.dense = nn.Linear(D_in, D_in)
         self.dropout = nn.Dropout(drop_rate)
         self.out_proj = nn.Linear(D_in, D_out)
-        self.flag_ = flag
 
     def forward(self, input_ids, attention_masks):
-        outputs = self.roberta(input_ids, attention_masks)
+        outputs = self.bert(input_ids, attention_masks)
         class_label_output = outputs[0][:,0]
         return class_label_output   
 
@@ -103,7 +102,7 @@ def violin_plot(X , prop, save_path, prop2): #Violin plots for the comparison of
 	plt.xlabel('Model', fontsize=40)
 	plt.xticks(fontsize=36)
 	plt.yticks(fontsize=32)
-	plt.ylabel('Property distance', fontsize=36)	
+	plt.ylabel('Ratio', fontsize=36)	
 	plt.title(f'{prop_}', fontsize=40)
 	#plt.legend(loc='center left', bbox_to_anchor=(0.35, -0.1), prop={'size': 12})
 	plt.tight_layout()
@@ -125,8 +124,8 @@ def tasks(prop,  save_path):
 	df = read_file(prop,  'chemlm')
 	df_mol = read_file(prop,  'molbert')
 	violin_plot( df.lip.tolist(), prop, save_path,  df_mol.lip.tolist())	
-	stats(prop, df2, 'chemlm')
-	stats(prop, df2_mol, 'molbert')
+	stats(prop, df, 'chemlm')
+	stats(prop, df_mol, 'molbert')
 
 def stats(prop, df, model_name_): #calculates the statistical values for table 3, page 14. It is the comparison for ChemLM and MolBERT on Lipscitz compounds
 	print(model_name_)
@@ -136,57 +135,57 @@ def stats(prop, df, model_name_): #calculates the statistical values for table 3
 	print(f'std value: {np.std(vals)}')
 	print(f'mad value: {scipy.stats.median_abs_deviation(vals)}')
 
-def calc_lip(props,save_path, m_path, mm_path, t_path, chemlm_emb): #calculates the ratio of property to embeddings distance for molecular pairs. This is for the ChemLM approach. The same method has been used in MolBERT on their code. 
-    if torch.cuda.is_available():
-    	device = torch.device("cuda")
-    	print("Using GPU.")
-    else:
-    	print("No GPU available, using the CPU instead.")
-    	device = torch.device("cpu")
-    current_directory = os.getcwd()
+def calc_lip(props,save_path, model_path, model_path_, t_path): #calculates the ratio of property to embeddings distance for molecular pairs. This is for the ChemLM approach. The same method has been used in MolBERT on their code. 
+	if torch.cuda.is_available():
+		device = torch.device("cuda")
+		print("Using GPU.")
+	else:
+		print("No GPU available, using the CPU instead.")
+		device = torch.device("cpu")
+	current_directory = os.getcwd()
 	target_folder = os.path.abspath(os.path.join(current_directory, os.pardir, os.pardir))
-	target_f = os.path.abspath(os.path.join(target_folder, 'data/intrinsic'))	
-    data = load_data_n('{target_f}/bbbp_comps_properties.csv')
-    model = Chemlm_model(dt_n='bbbp',drop_rate=0.1,an=80, att=12,n_layers= 8, tt='clf',seed_=3407,emb_type='last', m_p = m_path)
-    model.load_state_dict(torch.load(model_path_))
-   if device.type=='cpu':
-    	model.load_state_dict(torch.load('{}'.format(mm_path),map_location=torch.device('cpu')))
-    else:
-    	model.load_state_dict(torch.load('{}'.format(mm_path)))
-    model.eval()
+	target_f = os.path.abspath(os.path.join(target_folder, 'data/intrinsic'))
+	data = load_data_n('{target_f}/bbbp_comps_properties.csv')
+	model = Chemlm_model(dt_n='bbbp',drop_rate=0.1,an=80, att=12,n_layers= 8, tt='clf',seed_=3407,emb_type='last', mo_path = model_path)
+	model.load_state_dict(torch.load(model_path_))
+	model.to(device)
+	model.eval()
 
-    df=pd.DataFrame(columns=['distance', 'diffs', 'lip'])
-    for prop in props:
-    	print(f'Property: {prop}')
-    	con_max = -1
-    	for i in tqdm.tqdm(range(len(data))):
-    		for j in range(i):
-	    		dist = chemlm_emb(model,t_path, data.loc[i].smiles, data.loc[j].smiles, device)
-	    		abs_prop = calc_props(data.loc[i][prop], data.loc[j][prop])
-	    		con = abs_prop/dist
-	    		if con_max < con:
-	    			con_max = con
-	    			con_pair = (dist, abs_prop)
-	    		df = df.append({'distance':dist, 'diffs':abs_prop, 'lip':con}, ignore_index=True)
-    	df.to_csv(f'{save_path}/chemlm_chmb_{prop}.csv')
-    	del df
-    	gc.collect()
+	for prop in props:
+		df=pd.DataFrame(columns=['distance', 'diffs', 'lip'])
+		print(f'Property: {prop}')
+		con_max = -1
+		for i in tqdm.tqdm(range(len(data))):
+			for j in range(i):
+				dist = chemlm_emb(model,t_path, data.loc[i].smiles, data.loc[j].smiles, device)
+				abs_prop = calc_props(data.loc[i][prop], data.loc[j][prop])
+				con = abs_prop/dist
+				if con_max < con:
+					con_max = con
+					con_pair = (dist, abs_prop)
+				df = df.append({'distance':dist, 'diffs':abs_prop, 'lip':con}, ignore_index=True)
+				print('Prop')
+		df.to_csv(f'{save_path}/chemlm_chmb_{prop}.csv')
+		del df
+		gc.collect()
 
     	
 if __name__=='__main__':
 	props = [  'qed','psa', 'mw']
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--save_path', type=str, required=True)
-    parser.add_argument('--model_path', type=str, required=True)             
-    parser.add_argument('--tokenizer_path', type=str, required=True)
-    parser.add_argument('--mpath', type=str, required=True)
+	parser = argparse.ArgumentParser()
+	parser.add_argument('--save_path', type=str, required=True)
+	parser.add_argument('--model_path', type=str, required=True)
+	parser.add_argument('--tokenizer_path', type=str, required=True)
+	parser.add_argument('--mpath', type=str, required=True)
 
-    args= parser.parse_args()
-    save_p = args.save_path
-    m_path = args.model_path
-    t_path = args.tokenizer_path
-    mm_path = args.mpath
+	args= parser.parse_args()
+	save_p = args.save_path
+	m_path = args.model_path
+	t_path = args.tokenizer_path
+	mm_path = args.mpath
+	#calc_lips(props,save_p, m_path, mm_path, t_path)
 	for prop in props:
+		br
 		print('----------------')		
 		print(prop)
 		tasks(prop, save_p)
